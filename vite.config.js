deb9ff3f7c78eb0dc4819598a55ac7a2ce0efbc2
Muchyz <kelvinmuchiri699@gmail.com>
2026-02-28 19:17:33 +0300

    optimize build

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          charts: ['recharts'],
+          icons: ['lucide-react'],
+        }
+      }
+    },
+    chunkSizeWarningLimit: 1000,
+    minify: 'terser',
+  },
+  css: {
+    devSourcemap: false,
+  }
 })
